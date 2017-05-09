fe761261ba9bc6f3c6cbabca0ff843204a1b48ed
Markus Felten <Markus.Felten@gmx.de>
2017-05-09 13:17:54 +0200

    wip

--- src/url-util.js
+++ src/url-util.js
@@ -34,18 +34,16 @@ process.on('unhandledRejection', reason => spinner.fail(reason));
 program
   .description('work with url resources')
   .version(require(path.join(__dirname, '..', 'package.json')).version)
-  .option('-c, --config <file>', 'use config from file')
   .command('list', 'list url content')
+  .option('-c, --config <file>', 'use config from file')
   .argument('<url>', 'url to to list')
   .action(async(args, options, logger) => {
 
     const resolver = await prepareResolver(options);
 
-    async
-    for (entry of resoler.list(args.url)) {
+    for (const entry of resolver.list(args.url)) {
       console.log(entry);
     }
-
   });
 
 program.parse(process.argv);
@@ -53,26 +51,22 @@ program.parse(process.argv);
 
 async function prepareResolver(options) {
 
-  // default config if none is given
   const defaultConfig = {
     schemes: {
-      'tmp': {
+      tmp: {
         base: 'http',
         prefix: 'http:///tmp'
       }
     }
   };
 
-  // load config and expand expressions ${something} inside
   const config = await expand(options.config ? "${include('" + path.basename(options.config) + "')}" :
     defaultConfig, {
       constants: {
-        basedir: path.dirname(options.config || process.cwd()), // where is the config file located
-        installdir: path.resolve(__dirname, '..') // make references to the installdir possible
+        basedir: path.dirname(options.config || process.cwd()),
+        installdir: path.resolve(__dirname, '..')
       }
     });
 
-  const resolver = new Resolver(config, [HTTPScheme, HTTPSScheme, FileScheme, SVNHTTPSScheme]);
-
-  return resolver;
+  return new Resolver(config, [new HTTPScheme(), new HTTPSScheme(), /*new FileScheme(),*/ new SVNHTTPSScheme()]);
 }
