68bd62ab1382de03a1d44f081117b4b6f883f608
Markus Felten <Markus.Felten@gmx.de>
2017-05-24 23:22:42 +0200

    test: adopt url-cmd name

--- tests/run_test.js
+++ tests/run_test.js
@@ -8,6 +8,6 @@ import test from 'ava';
 const execa = require('execa');
 
 test('cli', async t => {
-  const result = await execa(path.join(__dirname, '..', '..', 'bin', 'url-util'), ['-h']);
+  const result = await execa(path.join(__dirname, '..', '..', 'bin', 'url-cmd'), ['-h']);
   t.regex(result.stdout, /--config <file>/);
 });
