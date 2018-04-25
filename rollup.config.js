104bcb291c50229196116570481d86cf407cba93
Markus Felten <Markus.Felten@gmx.de>
2018-04-25 22:49:07 +0200

    chore: use rollup-plugin-executable instead of chmod

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import pkg from './package.json';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import executable from 'rollup-plugin-executable';
 import json from 'rollup-plugin-json';
 
 export default {
@@ -9,7 +10,7 @@ export default {
     format: 'cjs',
     banner: '#!/usr/bin/env node'
   },
-  plugins: [resolve(), commonjs(), json()],
+  plugins: [resolve(), commonjs(), json(), executable()],
   external: [
     'assert',
     'os',
