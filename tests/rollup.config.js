9d03a8ddb6930d04a3e1089233d0b21441041282
Markus Felten <Markus.Felten@gmx.de>
2018-04-30 18:19:37 +0200

    chore(rollup): let test/rollup.config.js also generate bin/*

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,16 +1,42 @@
 import multiEntry from 'rollup-plugin-multi-entry';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import executable from 'rollup-plugin-executable';
+import json from 'rollup-plugin-json';
+import pkg from '../package.json';
 
-export default {
-  input: 'tests/**/*-test.js',
-  external: ['ava', 'path', 'os'],
-
-  plugins: [resolve(), commonjs(), multiEntry()],
-
-  output: {
-    file: 'build/bunde-test.js',
-    format: 'cjs',
-    sourcemap: true
+const external = [
+  'ava',
+  'assert',
+  'os',
+  'events',
+  'path',
+  'url',
+  'url-resolver-fs',
+  'fs-resolver-fs',
+  'svn-dav-fs',
+  'sftp-resolver-fs',
+  'config-expander'
+];
+export default [
+  {
+    output: {
+      file: pkg.bin['url-cmd'],
+      format: 'cjs',
+      banner: '#!/usr/bin/env node'
+    },
+    plugins: [resolve(), commonjs(), json(), executable()],
+    external,
+    input: pkg.module
+  },
+  {
+    input: 'tests/**/*-test.js',
+    external,
+    plugins: [resolve(), commonjs(), multiEntry()],
+    output: {
+      file: 'build/bunde-test.js',
+      format: 'cjs',
+      sourcemap: true
+    }
   }
-};
+];
