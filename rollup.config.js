4842c3dc845551f0e87f43a843b00260ed7861b8
Markus Felten <Markus.Felten@gmx.de>
2018-08-30 22:12:59 +0200

    fix(rollup): make use of rollup-plugin-cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,32 @@
-import cleanup from 'rollup-plugin-cleanup';
-import pkg from './package.json';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import executable from 'rollup-plugin-executable';
-import json from 'rollup-plugin-json';
+import cleanup from "rollup-plugin-cleanup";
+import pkg from "./package.json";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import executable from "rollup-plugin-executable";
+import json from "rollup-plugin-json";
 
 export default {
   output: {
-    file: pkg.bin['url-cmd'],
-    format: 'cjs',
-    banner: '#!/usr/bin/env node',
+    file: pkg.bin["url-cmd"],
+    format: "cjs",
+    banner: "#!/usr/bin/env node",
     interop: false
   },
-  plugins: [resolve(), commonjs(), json(), executable()],
+  plugins: [resolve(), commonjs(), json(), cleanup(), executable()],
   external: [
-    'assert',
-    'os',
-    'events',
-    'path',
-    'url',
-    'fs',
-    'crypto',
-    'url-resolver-fs',
-    'fs-resolver-fs',
-    'svn-dav-fs',
-    'sftp-resolver-fs',
-    'svn-simple-auth-provider',
-    'config-expander'
+    "assert",
+    "os",
+    "events",
+    "path",
+    "url",
+    "fs",
+    "crypto",
+    "url-resolver-fs",
+    "fs-resolver-fs",
+    "svn-dav-fs",
+    "sftp-resolver-fs",
+    "svn-simple-auth-provider",
+    "config-expander"
   ],
   input: pkg.module
 };
--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,42 +1,42 @@
-import istanbul from 'rollup-plugin-istanbul';
-import multiEntry from 'rollup-plugin-multi-entry';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import executable from 'rollup-plugin-executable';
-import json from 'rollup-plugin-json';
-import pkg from '../package.json';
-import baseRollup from '../rollup.config.js';
+import istanbul from "rollup-plugin-istanbul";
+import multiEntry from "rollup-plugin-multi-entry";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import executable from "rollup-plugin-executable";
+import json from "rollup-plugin-json";
+import pkg from "../package.json";
+import baseRollup from "../rollup.config.js";
 
 const external = [
-  'ava',
-  'execa',
-  'assert',
-  'os',
-  'events',
-  'path',
-  'url',
-  'url-resolver-fs',
-  'fs-resolver-fs',
-  'svn-dav-fs',
-  'sftp-resolver-fs',
-  'config-expander'
+  "ava",
+  "execa",
+  "assert",
+  "os",
+  "events",
+  "path",
+  "url",
+  "url-resolver-fs",
+  "fs-resolver-fs",
+  "svn-dav-fs",
+  "sftp-resolver-fs",
+  "config-expander"
 ];
 export default [
   baseRollup,
   {
-    input: 'tests/**/*-test.js',
+    input: "tests/**/*-test.js",
     external,
     plugins: [
       multiEntry(),
-      istanbul({
-        exclude: ['tests/**/*-test.js']
-      }),
       resolve(),
-      commonjs()
+      commonjs(),
+      istanbul({
+        exclude: ["tests/**/*-test.js"]
+      })
     ],
     output: {
-      file: 'build/bunde-test.js',
-      format: 'cjs',
+      file: "build/bunde-test.js",
+      format: "cjs",
       sourcemap: true,
       interop: false
     }
