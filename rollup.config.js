24ed9f8497e6dbb1fe0d0d095f0a048edf60c420
Markus Felten <Markus.Felten@gmx.de>
2018-10-25 00:56:32 +0200

    chore(rollup): use rollup-plugin-babel

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import pkg from "./package.json";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import executable from "rollup-plugin-executable";
+import babel from "rollup-plugin-babel";
 import json from "rollup-plugin-json";
 
 export default {
@@ -16,6 +17,22 @@ export default {
   plugins: [
     resolve(),
     commonjs(),
+    babel({
+      runtimeHelpers: false,
+      externalHelpers: true,
+      babelrc: false,
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: {
+              safari: "tp"
+            }
+          }
+        ]
+      ],
+      exclude: "node_modules/**"
+    }),
     json({
       include: "package.json",
       preferConst: true,
--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -2,6 +2,7 @@ import istanbul from "rollup-plugin-istanbul";
 import multiEntry from "rollup-plugin-multi-entry";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import babel from "rollup-plugin-babel";
 import baseRollup from "../rollup.config.js";
 
 const external = [
@@ -24,6 +25,24 @@ export default [
     input: "tests/**/*-test.js",
     external,
     plugins: [
+      babel({
+        runtimeHelpers: false,
+        externalHelpers: true,
+        babelrc: false,
+        plugins: ["@babel/plugin-proposal-async-generator-functions"],
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              targets: {
+                safari: "tp"
+              },
+              modules: false
+            }
+          ]
+        ],
+        exclude: "node_modules/**"
+      }),
       multiEntry(),
       resolve(),
       commonjs(),
