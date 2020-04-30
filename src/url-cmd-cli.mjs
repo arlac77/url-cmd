e2310cf9983fa390cede0b3be45694f3b622003e
Markus Felten <Markus.Felten@gmx.de>
2020-04-30 10:45:35 +0200

    fix: get rid of rollup

--- src/url-cmd-cli.mjs
+++ src/url-cmd-cli.mjs
@@ -1,12 +1,22 @@
 import program from "commander";
 import { expand } from "config-expander";
-import { version, description } from "../package.json";
-import { basename, dirname, resolve } from "path";
+import { join, basename, dirname, resolve } from "path";
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
 import { Resolver, HTTPScheme, HTTPSScheme } from "url-resolver-fs";
 import { FileScheme } from "fs-resolver-fs";
 import { SVNHTTPSScheme } from "svn-dav-fs";
 import { SFTPScheme } from "sftp-resolver-fs";
 import { SvnSimpleAuthProvider } from "svn-simple-auth-provider";
+
+const here = dirname(fileURLToPath(import.meta.url));
+
+const { version, description } = JSON.parse(
+  readFileSync(
+    join(here, "..", "package.json"),
+    { endoding: "utf8" }
+  )
+);
 
 program
   .description(description)
